a14984c944d31a1ee8acc42f58af77b86c92418a
Ross <rostikvunarchuk@gmail.com>
2022-04-24 23:04:28 +0300

    init commit

--- tests.py
+++ tests.py
@@ -1,1 +1,16 @@
-# import pytest
+from main import recoverSecret
+
+secret = "whatisup"
+triplets = [
+    ["t", "u", "p"],
+    ["w", "h", "i"],
+    ["t", "s", "u"],
+    ["a", "t", "s"],
+    ["h", "a", "p"],
+    ["t", "i", "s"],
+    ["w", "h", "s"],
+]
+
+
+def test1():
+    assert recoverSecret(triplets) == secret
